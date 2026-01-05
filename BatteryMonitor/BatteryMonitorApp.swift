d1b7ff5805ee9ec22f9260d963495f9bf74ec33a
mekabrine <119816853+mekabrine@users.noreply.github.com>
2026-01-05 07:16:07 -0700

    Update BatteryMonitorApp.swift

--- BatteryMonitor/BatteryMonitorApp.swift
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -2,11 +2,24 @@ import SwiftUI
 
 @main
 struct BatteryMonitorApp: App {
-    @StateObject private var monitor = DeviceMonitor()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    @StateObject private var monitor = DeviceMonitor.shared
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView(monitor: monitor)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                monitor.refreshNow()
+            case .background:
+                monitor.refreshNow()           // capture one right as we leave
+                monitor.scheduleBackgroundRefresh()
+            default:
+                break
+            }
         }
     }
 }
