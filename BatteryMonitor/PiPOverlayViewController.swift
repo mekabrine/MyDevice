5bc701d1f27430e778a714a9f92302efa6ba4791
mekabrine <119816853+mekabrine@users.noreply.github.com>
2026-01-04 23:46:32 -0700

    Update PiPOverlayViewController.swift

--- BatteryMonitor/PiPOverlayViewController.swift
+++ BatteryMonitor/PiPOverlayViewController.swift
@@ -5,18 +5,23 @@ final class PiPOverlayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
 
-        view.backgroundColor = .systemBackground
-
-        label.text = "BatteryMonitor"
-        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.font = .monospacedSystemFont(ofSize: 16, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(label)
-
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12),
         ])
+    }
+
+    func setText(_ text: String) {
+        label.text = text
     }
 }
