313556111c43299ac3bd4771d722d57fcab3abe0
mekabrine <119816853+mekabrine@users.noreply.github.com>
2026-01-05 07:41:59 -0700

    Update BatteryMonitorApp.swift

--- BatteryMonitor/BatteryMonitorApp.swift
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -3,11 +3,10 @@ import SwiftUI
 @main
 struct BatteryMonitorApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
-    @StateObject private var monitor = DeviceMonitor.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView(monitor: monitor)
+            ContentView(monitor: .shared)
         }
     }
 }
